c6d9857dac53fa7f6cbf07e74c427fb130908081
ljvmiranda921 <lester.miranda@obf.ateneo.edu>
2018-05-24 20:05:48 +0900

    Add base Topology class

---
+++ pyswarms/backend/topology/base.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+"""
+Base class for Topologies
+"""
+
+class Topology(object):
+
+    def __init__(self, **kwargs):
+        """Initializes the class"""
+        pass
+
+    def compute_best_particle(self, swarm):
+        """Computes the best particle of the swarm and returns the cost and
+        position"""
+        raise NotImplementedError("Topology::compute_best_particle()")
+
+    def update_position(self, swarm):
+        """Updates the swarm's position-matrix"""
+        raise NotImplementedError("Topology::update_position()")
+
+    def update_velocity(self, swarm):
+        """Updates the swarm's velocity-matrix"""
+        raise NotImplementedError("Topology::update_velocity()")
